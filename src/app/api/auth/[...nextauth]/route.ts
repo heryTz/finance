af2ff36269d026bd7c4edaba13bd196263ec2d5c
Nirintsoa <hhery889@gmail.com>
2023-08-30 01:15:45 +0300

    fix: smtp

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -10,7 +10,15 @@ export const authOptions: AuthOptions = {
   adapter: PrismaAdapter(prisma) as any,
   providers: [
     EmailProvider({
-      server: process.env.EMAIL_SERVER,
+      server: {
+        host: process.env.SMTP_HOST,
+        port: process.env.SMTP_PORT,
+        secure: true,
+        auth: {
+          user: process.env.SMTP_USER,
+          pass: process.env.SMTP_PASS,
+        },
+      },
       from: process.env.EMAIL_FROM,
     }),
   ],
