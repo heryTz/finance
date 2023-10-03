3b7c7f68d2adb7f3a714d7aece2755f2a56cca26
Nirintsoa <hhery889@gmail.com>
2023-10-03 12:31:12 +0300

    feat: active bottom nav

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -2,9 +2,12 @@
 import { PropsWithChildren, useState } from "react";
 import MuiAppBar, { AppBarProps as MuiAppBarProps } from "@mui/material/AppBar";
 import {
+  BottomNavigation,
+  BottomNavigationAction,
   Box,
   Container,
   Divider,
+  Hidden,
   IconButton,
   List,
   ListItemButton,
@@ -12,6 +15,7 @@ import {
   ListItemText,
   Menu,
   MenuItem,
+  Paper,
   Toolbar,
   Typography,
   styled,
@@ -27,6 +31,7 @@ import {
 import Link from "next/link";
 import { AdminGuard } from "../guards/admin-guard";
 import { signOut } from "next-auth/react";
+import { useRouter, usePathname } from "next/navigation";
 
 const drawerWidth: number = 240;
 
@@ -49,6 +54,12 @@ const AppBar = styled(MuiAppBar, {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.enteringScreen,
     }),
+    [theme.breakpoints.down("md")]: {
+      left: 0,
+      right: 0,
+      width: "100%",
+      marginLeft: 0,
+    },
   }),
 }));
 
@@ -79,6 +90,8 @@ const Drawer = styled(MuiDrawer, {
 }));
 
 function Component({ children }: PropsWithChildren<{}>) {
+  const router = useRouter();
+  const pathname = usePathname();
   const [anchorUser, setAnchorUser] = useState<null | HTMLElement>(null);
   const [open, setOpen] = useState(true);
 
@@ -87,109 +100,137 @@ function Component({ children }: PropsWithChildren<{}>) {
   };
 
   const openUserMenu = Boolean(anchorUser);
+  const menus = [
+    { label: "Dashboard", href: "/", icon: <Dashboard /> },
+    { label: "Finance", href: "/finance", icon: <AccountBalance /> },
+  ];
+  const tabsIndex = menus.findIndex((el) => el.href === pathname);
 
   return (
-    <Box sx={{ display: "flex" }}>
-      <AppBar position="absolute" open={open}>
-        <Toolbar sx={{ pr: "24px" }}>
-          <IconButton
-            edge="start"
-            color="inherit"
-            aria-label="open drawer"
-            onClick={toggleDrawer}
-            sx={{
-              marginRight: "36px",
-              ...(open && { display: "none" }),
-            }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography
-            component="h1"
-            variant="h6"
-            color="inherit"
-            noWrap
-            sx={{ flexGrow: 1 }}
-          >
-            Dashboard
-          </Typography>
-          <IconButton
-            color="inherit"
-            id="user-menu-button"
-            aria-controls={openUserMenu ? "user-menu" : undefined}
-            aria-haspopup="true"
-            aria-expanded={openUserMenu ? "true" : undefined}
-            onClick={(e) => setAnchorUser(e.currentTarget)}
-          >
-            <AccountCircle />
-          </IconButton>
-          {anchorUser && (
-            <Menu
-              id="user-menu"
-              anchorEl={anchorUser}
-              open={open}
-              onClose={() => setAnchorUser(null)}
-              MenuListProps={{
-                "aria-labelledby": "user-menu-button",
+    <>
+      <Box sx={{ display: "flex" }}>
+        <AppBar position="absolute" open={open}>
+          <Toolbar sx={{ pr: "24px" }}>
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="open drawer"
+              onClick={toggleDrawer}
+              sx={{
+                marginRight: "36px",
+                ...(open && { display: "none" }),
               }}
             >
-              <MenuItem key={"signout"} onClick={() => signOut()}>
-                Déconnexion
-              </MenuItem>
-            </Menu>
-          )}
-        </Toolbar>
-      </AppBar>
-      <Drawer variant="permanent" open={open}>
-        <Toolbar
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              component="h1"
+              variant="h6"
+              color="inherit"
+              noWrap
+              sx={{ flexGrow: 1 }}
+            >
+              Dashboard
+            </Typography>
+            <IconButton
+              color="inherit"
+              id="user-menu-button"
+              aria-controls={openUserMenu ? "user-menu" : undefined}
+              aria-haspopup="true"
+              aria-expanded={openUserMenu ? "true" : undefined}
+              onClick={(e) => setAnchorUser(e.currentTarget)}
+            >
+              <AccountCircle />
+            </IconButton>
+            {anchorUser && (
+              <Menu
+                id="user-menu"
+                anchorEl={anchorUser}
+                open={open}
+                onClose={() => setAnchorUser(null)}
+                MenuListProps={{
+                  "aria-labelledby": "user-menu-button",
+                }}
+              >
+                <MenuItem key={"signout"} onClick={() => signOut()}>
+                  Déconnexion
+                </MenuItem>
+              </Menu>
+            )}
+          </Toolbar>
+        </AppBar>
+        <Hidden mdDown>
+          <Drawer variant="permanent" open={open}>
+            <Toolbar
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "flex-end",
+                px: [1],
+              }}
+            >
+              <IconButton onClick={toggleDrawer}>
+                <ChevronLeft />
+              </IconButton>
+            </Toolbar>
+            <Divider />
+            <List component="nav">
+              {menus.map((el) => (
+                <Link
+                  key={el.href}
+                  href={el.href}
+                  style={{ textDecoration: "none", color: "inherit" }}
+                >
+                  <ListItemButton>
+                    <ListItemIcon>{el.icon}</ListItemIcon>
+                    <ListItemText primary={el.label} />
+                  </ListItemButton>
+                </Link>
+              ))}
+            </List>
+          </Drawer>
+        </Hidden>
+        <Box
+          component="main"
           sx={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "flex-end",
-            px: [1],
+            backgroundColor: (theme) =>
+              theme.palette.mode === "light"
+                ? theme.palette.grey[100]
+                : theme.palette.grey[900],
+            flexGrow: 1,
+            height: "100vh",
+            overflow: "auto",
           }}
         >
-          <IconButton onClick={toggleDrawer}>
-            <ChevronLeft />
-          </IconButton>
-        </Toolbar>
-        <Divider />
-        <List component="nav">
-          {[
-            { label: "Dashboard", href: "/", icon: <Dashboard /> },
-            { label: "Finance", href: "/finance", icon: <AccountBalance /> },
-          ].map((el) => (
-            <Link
-              key={el.href}
-              href={el.href}
-              style={{ textDecoration: "none", color: "inherit" }}
-            >
-              <ListItemButton>
-                <ListItemIcon>{el.icon}</ListItemIcon>
-                <ListItemText primary={el.label} />
-              </ListItemButton>
-            </Link>
-          ))}
-        </List>
-      </Drawer>
-      <Box
-        component="main"
-        sx={{
-          backgroundColor: (theme) =>
-            theme.palette.mode === "light"
-              ? theme.palette.grey[100]
-              : theme.palette.grey[900],
-          flexGrow: 1,
-          height: "100vh",
-          overflow: "auto",
-        }}
-      >
-        <Toolbar />
-        <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-          {children}
-        </Container>
+          <Toolbar />
+          <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+            {children}
+          </Container>
+        </Box>
       </Box>
-    </Box>
+      <Hidden mdUp>
+        <Paper
+          sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}
+          elevation={3}
+        >
+          <BottomNavigation
+            showLabels
+            value={tabsIndex}
+            onChange={(_, index) => {
+              router.push(menus[index].href);
+            }}
+          >
+            {menus.map((el) => (
+              <BottomNavigationAction
+                key={el.href}
+                label={el.label}
+                icon={el.icon}
+              />
+            ))}
+          </BottomNavigation>
+        </Paper>
+      </Hidden>
+    </>
   );
 }
 
