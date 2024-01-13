24c0c80ebb2a34ca1de820b926569b607ca13dbb
Nirintsoa <hhery889@gmail.com>
2024-01-14 00:39:51 +0300

    feat: jest server setup

--- jest.config.mjs
+++ jest.config.mjs
@@ -21,8 +21,22 @@ const clientConfig = {
 };
 
 /** @type {import('jest').Config} */
+const serverConfig = {
+  displayName: "server",
+  testEnvironment: "node",
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+  // setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  testMatch: ["**/*.spec.ts?(x)"],
+};
+
+/** @type {import('jest').Config} */
 const config = {
-  projects: [await createJestConfig(clientConfig)()],
+  projects: [
+    await createJestConfig(clientConfig)(),
+    await createJestConfig(serverConfig)(),
+  ],
 };
 
 export default config;
