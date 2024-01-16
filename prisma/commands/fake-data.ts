e298d7694896b95e914bfb1be72868435c4d547a
Nirintsoa <hhery889@gmail.com>
2024-01-16 15:50:45 +0300

    feat: improve fake data

--- prisma/commands/fake-data.ts
+++ prisma/commands/fake-data.ts
@@ -1,9 +1,8 @@
-import { PrismaClient } from "@prisma/client";
 import { faker } from "@faker-js/faker";
 import { FinanceType } from "../../src/entity";
+import { prisma } from "@/lib/prisma";
 
 async function run() {
-  const prisma = new PrismaClient();
   const finances = await prisma.finance.findMany();
   for (const finance of finances) {
     await prisma.finance.update({
@@ -23,6 +22,61 @@ async function run() {
     await prisma.tag.update({
       where: { id: tags[index].id },
       data: { name: faker.word.noun() + index },
+    });
+  }
+
+  const clients = await prisma.client.findMany();
+  for (let index = 0; index < clients.length; index++) {
+    await prisma.client.update({
+      where: { id: clients[index].id },
+      data: {
+        name: faker.animal.cat() + index,
+        address: faker.lorem.words(2),
+        ape: faker.number.int(100000).toString(),
+        email: faker.person.firstName() + "@yopmail.fr",
+        siren: faker.string.alpha(),
+        nif: faker.string.alpha(),
+        phone: faker.phone.number(),
+      },
+    });
+  }
+
+  const providers = await prisma.provider.findMany();
+  for (const provider of providers) {
+    await prisma.provider.update({
+      where: { id: provider.id },
+      data: {
+        address: faker.lorem.words(2),
+        ape: faker.number.int(100000).toString(),
+        email: faker.person.firstName() + "@yopmail.fr",
+        name: faker.person.fullName(),
+        siren: faker.string.alpha(),
+        nif: faker.string.alpha(),
+        phone: faker.phone.number(),
+      },
+    });
+  }
+
+  const products = await prisma.product.findMany();
+  for (const product of products) {
+    await prisma.product.update({
+      where: { id: product.id },
+      data: {
+        name: faker.commerce.product(),
+        price: +faker.commerce.price(),
+      },
+    });
+  }
+
+  const paymentsInfo = await prisma.paymentMode.findMany();
+  for (const payment of paymentsInfo) {
+    await prisma.paymentMode.update({
+      where: { id: payment.id },
+      data: {
+        accountName: faker.animal.crocodilia(),
+        iban: faker.string.alphanumeric(),
+        name: faker.commerce.department(),
+      },
     });
   }
 
