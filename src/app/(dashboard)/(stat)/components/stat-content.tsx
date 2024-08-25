00e1df432d4c39d9c5482ee960bad32d0cf7e706
Hery Nirintsoa <hhery889@gmail.com>
2024-08-25 10:38:53 +0300

    feat: renome line-chart folder

--- src/app/(dashboard)/(stat)/components/stat-content.tsx
+++ src/app/(dashboard)/(stat)/components/stat-content.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { LineChart } from "@/components/chart/line-chart";
+import { LineChart } from "@/components/line-chart";
 import { GetStats } from "../stat-service";
 
 const labels = [
