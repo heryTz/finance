e112629b262d5bb56df10cee5833764cd82379ed
Hery Nirintsoa <hhery889@gmail.com>
2024-08-27 15:46:11 +0300

    feat: improve calendar field

--- src/components/calendar-field.tsx
+++ src/components/calendar-field.tsx
@@ -5,7 +5,7 @@ import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
 import { Button } from "./ui/button";
 import { cn } from "@/lib/utils";
 import { humanDate } from "@/lib/humanizer";
-import { CalendarIcon } from "lucide-react";
+import { CalendarDaysIcon } from "lucide-react";
 
 export const CalendarField = forwardRef<HTMLDivElement, CalendarFieldProps>(
   ({ placeholder, value, label, onChange }, ref) => {
@@ -18,16 +18,16 @@ export const CalendarField = forwardRef<HTMLDivElement, CalendarFieldProps>(
               <Button
                 variant={"outline"}
                 className={cn(
-                  "w-full pl-3 text-left font-normal aria-[invalid=true]:border-destructive aria-[invalid=true]:text-destructive",
+                  "flex justify-start w-full pl-3 text-left font-normal aria-[invalid=true]:border-destructive aria-[invalid=true]:text-destructive",
                   !value && "text-muted-foreground",
                 )}
+                StartIcon={CalendarDaysIcon}
               >
                 {value ? (
                   humanDate(value)
                 ) : (
                   <span>{placeholder ?? "Prend une date"}</span>
                 )}
-                <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
               </Button>
             </FormControl>
           </PopoverTrigger>
