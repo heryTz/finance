c5a9bc8b1172643143834bdd07ddf2c280c2c3f0
Hery Nirintsoa <hhery889@gmail.com>
2024-09-03 21:43:16 +0300

    fix: react-modal

--- src/components/ui/react-modal.tsx
+++ src/components/ui/react-modal.tsx
@@ -87,7 +87,7 @@ ReactModalClose.displayName = "ReactModalClose";
 export const ReactModalContent = forwardRef<
   ReactModalPrimitive,
   PropsWithChildren<{ className?: string }>
->((props, ref) => {
+>(({ className, ...props }, ref) => {
   const context = useContext(ReactModalContext);
   return (
     <ReactModalPrimitive
@@ -95,9 +95,10 @@ export const ReactModalContent = forwardRef<
       overlayClassName={
         "fixed inset-0 z-50 bg-black/80  data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0"
       }
-      className={
-        "fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg"
-      }
+      className={cn(
+        "fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg focus-visible:outline-none duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg",
+        className,
+      )}
       aria={{
         labelledby: context.labelledby,
         describedby: context.describeby,
@@ -105,6 +106,7 @@ export const ReactModalContent = forwardRef<
       {...props}
       isOpen={!!context.open}
       onRequestClose={() => context.onOpenChange(false)}
+      data-open={context.open ? "open" : "closed"}
     >
       {props.children}
       <ReactModalClose className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground">
@@ -117,6 +119,17 @@ export const ReactModalContent = forwardRef<
 
 ReactModalContent.displayName = "ReactModalContent";
 
+export const ReactModalBody = ({
+  className,
+  children,
+  ...props
+}: ComponentProps<"div">) => {
+  return (
+    <div className={cn("px-4 md:px-0", className)} {...props}>
+      {children}
+    </div>
+  );
+};
 export const ReactModalHeader = forwardRef<
   HTMLDivElement,
   ComponentProps<"div">
