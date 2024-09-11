0f3e4dddceb52bfb949b696116378a7c1b574682
Hery Nirintsoa <hhery889@gmail.com>
2024-09-11 13:42:46 +0300

    chore: remove unused code

--- src/app/(dashboard)/invoice/client/client-query.ts
+++ src/app/(dashboard)/invoice/client/client-query.ts
@@ -3,12 +3,6 @@ import { Client } from "@prisma/client";
 import { useQuery } from "react-query";
 import { GetClients } from "./client-service";
 
-export function useGetClient() {
-  return useQuery(useGetClient.name, () =>
-    httpClient.get<GetClients>("/invoice/client"),
-  );
-}
-
 export function useGetClientById(id?: string | null) {
   return useQuery({
     queryKey: [useGetClientById.name, id],
--- src/lib/useUser.tsx
+++
@@ -1,8 +0,0 @@
-import { useSession } from "next-auth/react";
-
-export function useUser() {
-  const session = useSession();
-  if (!session?.data?.user)
-    throw new Error("You should wrap this hook with AppProvider");
-  return session.data.user;
-}
