309cb4723256686311a06811ed4be22a77907f3c
Hery Nirintsoa <hhery889@gmail.com>
2025-05-06 17:12:22 +0300

    fix: cannot download invoice

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
+const path = require("path");
+
+// html2canvas does not support "oklch" color
+// https://github.com/niklasvh/html2canvas/issues/2700#issuecomment-2799012449
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      html2canvas: path.resolve(__dirname, "node_modules/html2canvas-pro"),
+    };
+    return config;
+  },
   experimental: {
     turbo: {
       resolveAlias: {
